6418375bf06e0b0d43228dd9bf7fb2711af6a018
jaafarhamza <jaafar.hamza711@gmail.com>
2024-11-15 16:57:30 +0100

    last commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/*.html', './src/public/*.{js,jsx,ts,tsx}'],
+  content: ['./public/**/*.{html,js,css}'],
   theme: {
     extend: {},
   },
   plugins: [],
   darkMode: 'media',
-  // ...
+  safelist: [
+    'bg-blue-500',
+    'bg-gray-500',
+    'text-red-500',
+    'hover:bg-green-500', 
+  ],
 };
